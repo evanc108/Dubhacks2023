a752094fe8b3b985d8985fefa6728e9ef458f1d2
evanc108 <104491330+evanc108@users.noreply.github.com>
2023-10-15 00:26:12 -0700

    change to html js

---
+++ public/config/appEnvConfig.js
@@ -0,0 +1,67 @@
+/////////////////////////////////////////////////////
+// Client Config
+////////////////////////////////////////////////////
+export const clientConfig = {
+    networkEnvironment: {
+      cpaasURLBase: 'https://naas.t-mobile.com',
+      euiURLBase: 'https://account.t-mobile.com',
+      networkPingServer: 'https://digits.t-mobile.com',
+    },
+    clientId: 'tmon-CEeWDEq8QSUe1HyXyLwsFzAhEAa6y7yn', // Copy and Paste CPaaS clientId between the quote marks
+    clientURLBase: 'http://localhost:8080', // Paste your applications redirection URL between the quote marks
+    clientSecret: 's1HJ1f31sJAAPbXG', // Copy and Paste CPaaS secret between the quote marks
+    clientPrivateKey: '-----BEGIN OPENSSH PRIVATE KEY-----' +
+    'b3BlbnNzaC1rZXktdjEAAAAACmFlczI1Ni1jdHIAAAAGYmNyeXB0AAAAGAAAABCiUc4E7J' +
+    'cBApERDu8MjCHLAAAAEAAAAAEAAAGXAAAAB3NzaC1yc2EAAAADAQABAAABgQCffYD9Z6EE'+
+    'VLsDLPsR2JTRqbUsztoH6kQWCAVY2Svx+rBEAFfjVa5+k5m3wK//W5+ic9hfqPqYwhId8D'+
+    '/YBcJrzljenmff75l//z4MLE0jsb9SGtvmmcqpm2Zf4OPmAw+bgmepUNDEmBTO8ORaiuim'+
+    'tnVunfbJVvHxtadafbQNEIBhmPWsn/IStdeFILswXkjQi3WJhgILicm56Nb0Sq/YSD1PWZ'+
+    'IzdHzm7KgNlEb6CfFNxzwlaJAARV5m8JRLQ91AlvfA6Y1ag4meKJJpwbK8UAdjDNMCxJEJ'+
+    'sJ1CxBFpEFbFAvohU2QTQWjLbJtosYbuJldn2v1EroORFw3VfDPlu3iclO3+87as47D3p4'+
+    'Lz1FqxA5yvNrWVLRac5LjDlARG4jPJOjYIJLOpWqWPsqhbulntDn7rKymNEf0qBwmH1SOI'+
+    'GsZQnx24B0V1x7KLEWff0hH/m0bFWlxTzJWVo0AOwUCP/VmvuJvwxgXmHAhaHvGSSAt+En'+
+    '04jjJmSuBXY+cAAAWgFcsmsLfnEyflKKQQ7ccHdLpAKJZgCrpl6mNUxQTGIflaoKRMv+lB'+
+    'AYJKv6mys1atm+b4FFBP/Lz9JUEAG9u6MDdhwMXPfyGdLuoJHOCMxJd9PAHAq9eUFgvjK/'+
+    '7AfyYhbgToilDXxpKlOCF/Fdfj75bSHlAP6cxxQM6Z1a/CgxaZnijAm5J3U2LFJgAITJyc'+
+    'TLxZ7XBc3qy4SrBIXT8ubcV8xsvCEGyLgLWUn1r4wlGFLv95fFqrBB7J4GEPPbnLxTrtBc'+
+    'ZBO79bTeTsegKDdgQlZsI7UfX1KSDkSFEjEPE6cCO6J8fWZvW4MX61fBFlHDCwAdyVvgL2'+
+    'rumVqFuf4QG++kCzgOZ9pJ+O01s/wU9C09Qx5EUuu7mpU5ID/hVTH2CvHCAfzwy0lySQRq'+
+    't/eOVZJDuH6LMEQ6JfRt0SQxGBRQHxTOPtvqOm4Z8oMSEk6YLOf2NursDcEHyUMOk6N1Sa'+
+    'wQ3jXWxz3yNqOEp2atEmz8oXJDncpXwM5Csma1drvQpJOB/uHcOKQFS/mwi7yOG1lFqydZ'+
+    '8qSPoSbZBAFy30onr/mAjAEkihfGZVf6fWNKlErP0RbiovD2NhWOE1k/X6cgCpxh5R35sw'+
+    'k0p61lKJgMPRVCAjKQ6fahdlK32KzkydAsJTYfAgqW5e1N7RqwybULCI1Oo1YXd2hQBX5B'+
+    'S+TJ5D2D+z9HBCthrGXzG9fv9+ZNJdXB+7XNVDJyhdd6IP6GHEzpbWY/sDmHT+HjTw+7Bg'+
+    '31Tw1r+dHf7cvjpFXGNnK0R9svBl2OACSpkIbJdQehK1TuehRZd7vlZkn0Pp0X3HwkrCNP'+
+    'd6WbzzuM5K+kkDue2O3eOdUuZo7jJKH+TBlAWs201ywT07ExLZoOnLAtVXCFyfavQiK3xS'+
+    'ImFMhzFTNOzNqZBUfvs5ksEPQ6TJykPJ1oVolv8/lFLyVlQw2oF+Olwf732S/1nE/WvZpS'+
+    'nvhAsmx9lwamIAg5iRsE8JxqzDk3Rlx6LueZU6Tpb5OvwVitaujlTCmBihpq58jVWL1pdY'+
+    'D02WZC+KJsw7WvV0QI0+x/eHY88ZmXVMgrp3OcaEjiQ0a+KsH1A4UeMib/53LU6i1rAuhN'+
+    'bQOoM9y5fsfzsQhsl8FHGQrxIFdvKtgQHvuHQ8fXX0IUkKF3uxErCIgVj8ft6sdYRDVHhI'+
+    'gYO4h4JTN7TFO6oDMO1G0WaCFsI0s8PG2g2YPvD+uKPbesfMrMfmVhe4PLglw6e4O13JIN'+
+    'SsmJvuJ2R/FSXHsN5j7VuCVa0RFtiZdtS8miSUz6kwEhR7Mrn+yYQWn1CoZUdA0fb76KGT'+
+    'SF4pvHEkdm4m0txe2zh9BewCspDJyVMlzhQ9MQPKHb0psI8TxVRVCn6z8h7FUw3j4uNzIw'+
+    '4W7Q/zIE8VAuHYD3vyO0S5q7rZ9qLEvWDi8MhK80/Fxk2k3CEyCGgAQgS1laTpa5jdfAFF'+
+    'tON7Ek2mLLm5vDCUEk4CjigTuUVUzOFildcA0qvUSv1AAww2oZchEqsJZEsnq7lYs6UVDl'+
+    'fo0UpgwDM1m7CwzHWyQm9Jd5QgirIVz3p6ZLKYpv41ShZAOm/FCNgU8PJwUFVABZj2q4Li'+
+    'jX3EHX9M9mM6egkJpMwuLde8AsQDuh14JT5uXVGRlbJZOmVKrbezjL09O0zasNoSlCb93+'+
+    'XGbOHQ7Uaqszf8QYrdRaYjb7ryVbS9Uu+3nZQElBD8/fPv04X9m/yloxcKxzzZS0wRjkp6'+
+    '8ukLmAMLjuV4I+2F1b3ns/GxP/Kl0P0ZuGNu9cmQoX1iEb0lhfhW6elOr/VcSyAi3em+Pi'+
+    'RwM7oAX6/kWzr9n/w+eIiobayiVh/lArToNss38SVQrqrYjMXdya8O4udtGvHHq2IwWUSX'+
+    'w3JZ3BKgMJjgUsrtYAaGybfVY7m4Rh/He6SRMO6b+QXfojYj'+
+    '-----END OPENSSH PRIVATE KEY-----',
+    
+  }
+  /////////////////////////////////////////////////////
+  // Calling Config
+  ////////////////////////////////////////////////////
+  export const callingConfig = {
+    autoAcceptVideoUpgrade: {
+      enabled: false, // false by default
+    },
+    callLogStorage: 'indexed-db',
+    dtmfFolderURLPrefix: './soundFiles/tones',
+    ringbackToneURL: './soundFiles/internal_telephone_ring.ogg',
+    ringToneURL: './soundFiles/T-Mobile_RingTone.ogg',
+    turnServerDomain: 'stun.l.google.com',
+    turnServerPort: 19302,
+  }
